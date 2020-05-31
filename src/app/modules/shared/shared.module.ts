f472da98349d1d4ede84a6fe77c3bf4b697a4e27
Joshua Zinkovsky <joshua.zky@gmail.com>
2020-05-31 11:07:50 -0400

    Migrate footer to work as an angular component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,14 +2,21 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [NavigationComponent],
-  declarations: [NavigationComponent],
+  exports: [
+    FooterComponent,
+    NavigationComponent
+  ],
+  declarations: [
+    FooterComponent,
+    NavigationComponent
+  ],
   providers: [],
 })
 export class SharedModule { }
