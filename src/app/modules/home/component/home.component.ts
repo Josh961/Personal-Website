0b3c7110b2378b04a811bc4dc61fba55ce1ccfb1
Joshua Zinkovsky <joshua.zky@gmail.com>
2020-09-07 19:34:14 -0400

    Add theme styles to all project components
    Configure all components to use css variables for many of their colors.
    These variables get modified whenever the body tag gets a different
    class applied to it. Either:
      - light
      - dark
      - solarized
    
    The home page will set it's background images to match the current
    theme. If a theme is selected while still on the home page, the
    theme service will emit an item to its subject. This subject is
    subscribed to in the home.component.ts file which will add and
    remove the necessary classes on the top level home div.

--- src/app/modules/home/component/home.component.ts
+++ src/app/modules/home/component/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, Renderer2, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ThemeService } from 'src/app/services/theme-service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 
-export class HomeComponent implements OnInit {
-  constructor() { }
+export class HomeComponent implements OnDestroy, AfterViewInit {
+  @ViewChild('home') home: ElementRef;
+  private ngUnsubscribe = new Subject();
 
-  ngOnInit() { }
+  constructor(
+    private renderer: Renderer2,
+    private themeService: ThemeService
+  ) { }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  ngAfterViewInit(): void {
+    this.peformInitialThemeCheck();
+    this.subscribeToThemeService();
+  }
+
+  private peformInitialThemeCheck(): void {
+    const lightThemeSet = document.body.classList.contains('light');
+    const darkThemeSet = document.body.classList.contains('dark');
+
+    if (lightThemeSet) {
+      this.home.nativeElement.classList.remove('home--dark');
+      this.home.nativeElement.classList.remove('home--solarized');
+    } else if (darkThemeSet) {
+      this.renderer.addClass(this.home.nativeElement, 'home--dark');
+      this.home.nativeElement.classList.remove('home--solarized');
+    } else {
+      this.renderer.addClass(this.home.nativeElement, 'home--solarized');
+      this.home.nativeElement.classList.remove('home--dark');
+    }
+  }
+
+  private subscribeToThemeService(): void {
+    this.themeService.getTheme()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(res => {
+        if (res === 'light') {
+          this.renderer.removeClass(this.home.nativeElement, 'home--dark');
+          this.renderer.removeClass(this.home.nativeElement, 'home--solarized');
+        } else if (res === 'dark') {
+          this.renderer.addClass(this.home.nativeElement, 'home--dark');
+          this.renderer.removeClass(this.home.nativeElement, 'home--solarized');
+        } else if (res === 'solarized') {
+          this.renderer.addClass(this.home.nativeElement, 'home--solarized');
+          this.renderer.removeClass(this.home.nativeElement, 'home--dark');
+        }
+      });
+  }
 }
