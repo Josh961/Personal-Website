7dd619fb01b969d2d92787120070e65798acff1d
Joshua Zinkovsky <joshua.zky@gmail.com>
2020-09-13 11:58:48 -0400

    Add wildcard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,5 +25,9 @@ export const routes: Routes = [
   {
     path: 'contact',
     loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
   }
 ];
